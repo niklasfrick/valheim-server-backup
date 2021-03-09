dbffaa45a5781b2445be489598c3d50380a80304
Niklas Frick <hi@niklasfrick.com>
2021-03-09 08:25:34 +0100

    Update vhserverbackup.sh

--- vhserverbackup.sh
+++ vhserverbackup.sh
@@ -7,19 +7,19 @@ now=$(date +"%d-%m-%Y")
 
 # Sending Stopping notifications
 
-/usr/local/bin/discord.sh --webhook-url="${discordwebhook}" --text "@everyone ATTENTION: Valheim server will stop in 10 minutes for backup. Please log out!"
+/usr/local/sbin/discord.sh --webhook-url="${discordwebhook}" --text "@everyone ATTENTION: Valheim server will stop in 10 minutes for backup. Please log out!"
 
 sleep 5m
 
-/usr/local/bin/discord.sh --webhook-url="${discordwebhook}" --text "@everyone ATTENTION: Valheim server will stop in 5 minutes for backup. Please log out!"
+/usr/local/sbin/discord.sh --webhook-url="${discordwebhook}" --text "@everyone ATTENTION: Valheim server will stop in 5 minutes for backup. Please log out!"
 
 sleep 4m
 
-/usr/local/bin/discord.sh --webhook-url="${discordwebhook}" --text "@everyone ATTENTION: Valheim server stops in 1 minute for backup. Log out NOW!"
+/usr/local/sbin/discord.sh --webhook-url="${discordwebhook}" --text "@everyone ATTENTION: Valheim server stops in 1 minute for backup. Log out NOW!"
 
 sleep 1m
 
-/usr/local/bin/discord.sh --webhook-url="${discordwebhook}" --text "Valheim Server stops now!"
+/usr/local/sbin/discord.sh --webhook-url="${discordwebhook}" --text "Valheim Server stops now!"
 
 # Stop the Service
 systemctl stop valheimserver.service
@@ -34,7 +34,7 @@ cp /home/steam/.config/unity3d/IronGate/Valheim/worlds/${worldname}.db /home/ste
 cp /home/steam/.config/unity3d/IronGate/Valheim/worlds/${worldname}.fwl /home/steam/backups/${now}/${worldname}.fwl
 
 # clear backups older than 30 days
-/usr/local/bin/vhclearbackups.sh
+/usr/local/sbin/vhclearbackups.sh
 
 # Start the Service
 systemctl start valheimserver.service
@@ -42,4 +42,4 @@ systemctl start valheimserver.service
 sleep 30s
 
 # check if service is running and post to discord
-systemctl is-active --quiet valheimserver.service >/dev/null 2>&1 && /root/scripts/discord.sh --webhook-url="${discordwebhook}" --text "Valheim Server back ONLINE! Happy gaming!" || /usr/local/bin/discord.sh --webhook-url="${discordwebhook}" --text "Valheim Server not started. Please check..."
+systemctl is-active --quiet valheimserver.service >/dev/null 2>&1 && /usr/local/sbin/discord.sh --webhook-url="${discordwebhook}" --text "Valheim Server back ONLINE! Happy gaming!" || /usr/local/sbin/discord.sh --webhook-url="${discordwebhook}" --text "Valheim Server not started. Please check..."
